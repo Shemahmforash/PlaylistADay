464c0ff83c55c447e77085388a18c510cdcbd734
Carlos Rosão <shemahmforash@gmail.com>
2013-07-08 21:49:54 +0100

    Added place for json endpoint

--- playlistaday.py
+++ playlistaday.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<path:date>')
 def main(date=None):
     return render_template('base.html', date=date)
+
+@app.route('/getevents/')
+@app.route('/getevents/<date>')
+def events(date=None):
+    return jsonify()
 
 if __name__ == '__main__':
     app.debug = True
